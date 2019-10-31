e606b2ca14a4fa8effaa90a7243d3a4f24b69653
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-31 06:49:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -80,7 +80,7 @@ class Dog
       FROM dogs
       WHERE name = ?
     SQL
-    dog = DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end
   end
